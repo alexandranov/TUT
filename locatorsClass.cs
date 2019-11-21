d6c6c77bee0b5c2bc6af0da6331182a36d5ee2d0
alexandranov <darkday97@mail.ru>
2019-11-21 12:20:11 +0300

    Add files via upload

---
+++ locatorsClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using OpenQA.Selenium;
+using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+
+namespace AutomationTut.by
+{
+    [TestFixture]
+    public class Locators
+    {
+
+       //private IWebDriver driver;
+        [SetUp]
+        public void Setup()
+        {
+            
+         //   driver = new ChromeDriver(@"/Users/aleksandra/Documents/chromedrivers/");
+            
+
+        }
+
+
+        public Locators()
+        {
+            //var login_button = driver.FindElement(By.XPath("//a[@data-target-popup ='authorize-form']"));
+            //var login_field = driver.FindElement(By.XPath("//input[@name='login']"));
+            //var password_field = driver.FindElement(By.XPath("//input[@type='password']"));
+            //var enter_button = driver.FindElement(By.XPath("//input[@class ='button auth__enter']"));
+            //var name_button = driver.FindElement(By.XPath("//span[@class ='uname']"));
+            //var mail_button = driver.FindElement(By.XPath("//div[@id='authorize']/div/div/div/div/ul/li[2]/a"));
+            //var write_button = driver.FindElement(By.XPath("//span[@class = 'mail-ComposeButton-Text']"));
+        }
+    }
+}
